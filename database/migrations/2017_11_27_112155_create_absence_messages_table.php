2bef778e072f0411fbafa643ddfaac663b374e02
Peter Bork <pbn@intelligentbanker.com>
2017-11-27 12:30:35 +0100

    DB migrations

---
+++ database/migrations/2017_11_27_112155_create_absence_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAbsenceMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('absence_messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('message');
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('absence_messages');
+    }
+}
---
+++ database/migrations/2017_11_27_112741_create_attendances_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->increments('id');
+            $table->double('latitude');
+            $table->double('longitude');
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
